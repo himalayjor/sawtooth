a032d983e10a5aa09ee8df10666be64da202839a
Joriwal <himalayj@amazon.com>
2018-06-20 16:05:24 -0700

    Made get command

--- SimpleCounter/client/src/App.js
+++ SimpleCounter/client/src/App.js
@@ -1,6 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const {
+  signer,
+  BatchEncoder,
+  TransactionEncoder
+} = require('sawtooth-sdk/client');
+
+const privateKey = "63769fa8776a8ec66e7ec98d74ae70ed1497e890c824ce320ef56c9356721483";
+const publicKey = "02b87e4069985b34e414f6d090819a00371d523e98fd29bca0da7f6ec7910dd51a";
+
+const PREFIX = 'bf4e85';
+
+// Submit signed Transaction to validator
+// const submitUpdate = (payload, privateKey, cb) => {
+//   const transaction = new TransactionEncoder(privateKey, {
+//     inputs: [PREFIX],
+//     outputs: [PREFIX],
+//     familyName: FAMILY,
+//     familyVersion: VERSION,
+//     payloadEncoding: 'application/json',
+//     payloadEncoder: p => Buffer.from(JSON.stringify(p))
+//   }).create(payload)
+
+//   const batchBytes = new BatchEncoder(privateKey).createEncoded(transaction)
+
+  var myInit = { method: 'GET' };
+
+var myRequest = new Request('localhost:8008/state');
+
+fetch(myRequest,myInit).then(function(response) {
+  console.log(response);
+});
+
+//   $.post({
+//     url: `${API_URL}/batches?wait`,
+//     data: batchBytes,
+//     headers: {'Content-Type': 'application/octet-stream'},
+//     processData: false,
+//     // Any data object indicates the Batch was not committed
+//     success: ({ data }) => cb(!data),
+//     error: () => cb(false)
+//   })
+// }
 
 class App extends Component {
   render() {
@@ -19,3 +61,4 @@ class App extends Component {
 }
 
 export default App;
+
