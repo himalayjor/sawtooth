9bcc721c6bdc26523f7e76eb81ae10f60a536ae6
Joriwal <himalayj@amazon.com>
2018-06-22 07:08:15 -0700

    Revert "add get call to loadbalancer"
    This reverts commit 4e5e1f0de3ab5eb427211f3461479cf40a3f5c18.

--- SimpleCounter/client/src/App.js
+++ SimpleCounter/client/src/App.js
@@ -6,57 +6,43 @@ const {
   BatchEncoder,
   TransactionEncoder
 } = require('sawtooth-sdk/client');
-var request = require('request');
 
 const privateKey = "63769fa8776a8ec66e7ec98d74ae70ed1497e890c824ce320ef56c9356721483";
 const publicKey = "02b87e4069985b34e414f6d090819a00371d523e98fd29bca0da7f6ec7910dd51a";
 
 const PREFIX = 'bf4e85';
-const FAMILY = 'simple-counter';
-const VERSION = '1.0.0';
 
-const API_URL = 'http://sawtooth-counter-rest-api-1412363039.us-west-2.elb.amazonaws.com';
+// Submit signed Transaction to validator
+// const submitUpdate = (payload, privateKey, cb) => {
+//   const transaction = new TransactionEncoder(privateKey, {
+//     inputs: [PREFIX],
+//     outputs: [PREFIX],
+//     familyName: FAMILY,
+//     familyVersion: VERSION,
+//     payloadEncoding: 'application/json',
+//     payloadEncoder: p => Buffer.from(JSON.stringify(p))
+//   }).create(payload)
 
-// request(`${API_URL}/state`,  function (error, response, body) {
-//  console.log(response);
-//  console.log(error);
-//  console.log(body);
-// });
+//   const batchBytes = new BatchEncoder(privateKey).createEncoded(transaction)
 
-//Submit signed Transaction to validator
-const submitUpdate = (payload, privateKey) => {
-  const transaction = new TransactionEncoder(privateKey, {
-    inputs: [PREFIX],
-    outputs: [PREFIX],
-    familyName: FAMILY,
-    familyVersion: VERSION,
-    payloadEncoding: 'application/json',
-    payloadEncoder: p => Buffer.from(JSON.stringify(p))
-  }).create(payload);
+  var myInit = { method: 'GET' };
 
-  const batchBytes = new BatchEncoder(privateKey).createEncoded(transaction)
+var myRequest = new Request('localhost:8008/state');
 
-  console.log('something');
-
-request({
- 
-  uri: `${API_URL}/batches?wait`,
-  body: batchBytes,
-  method: 'POST'
-}, function (error, response, body) {
-  console.log('HelloWorld');
+fetch(myRequest,myInit).then(function(response) {
   console.log(response);
-  console.log(error);
-  console.log(body);
- });
-};
+});
 
-const payload = {
-  action: 'createAsset',
-  asset: 'FirstAsset'
-};
-
-submitUpdate(payload, privateKey);
+//   $.post({
+//     url: `${API_URL}/batches?wait`,
+//     data: batchBytes,
+//     headers: {'Content-Type': 'application/octet-stream'},
+//     processData: false,
+//     // Any data object indicates the Batch was not committed
+//     success: ({ data }) => cb(!data),
+//     error: () => cb(false)
+//   })
+// }
 
 class App extends Component {
   render() {
